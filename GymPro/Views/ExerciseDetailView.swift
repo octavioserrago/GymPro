f94346ec8ee9c7dca5e4c2469631eed833982bce
octavioserrago <octavioserrago.dev@gmail.com>
2024-02-27 16:56:38 -0300

    Added exercise to a day funcionality. More work need to be done

---
+++ GymPro/Views/ExerciseDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseDetailView.swift
+//  GymPro
+//
+//  Created by Octavio Serrago on 27/02/2024.
+//
+
+import SwiftUI
+
+struct ExerciseDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseDetailView()
+}
