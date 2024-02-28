b0ab225a7740631b4d082d6ac473556f89eea716
octavioserrago <octavioserrago.dev@gmail.com>
2024-02-27 21:41:44 -0300

    Started testing it, there's a lot of refactoring to do but it's a pretty basic MVP version

---
+++ GymPro/Views/AddSetsView.swift
@@ -0,0 +1,18 @@
+//
+//  AddSetsView.swift
+//  GymPro
+//
+//  Created by Octavio Serrago on 27/02/2024.
+//
+
+import SwiftUI
+
+struct AddSetsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddSetsView()
+}
